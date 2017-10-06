a9d913bfb674f9639d9f06cd46174579a8d9e9c8
Felipe Cuevas <felipe@softwareallies.com>
2017-10-06 12:29:43 -0500

    Create the basic CRUD controller, TODO: field and save records

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -1,2 +1,29 @@
 class RidersController < ApplicationController
+  def index
+  end
+
+  def new
+    @rider = Rider.new
+  end
+
+ 
+  def create
+    #TODO save a new rider
+    # redirect to show
+    redirect_to "/"
+  end
+
+  def show
+    #TODO: SHOW A RIDER IFORMATION
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    #TODO: destroy if don't have any relationship
+  end
 end
