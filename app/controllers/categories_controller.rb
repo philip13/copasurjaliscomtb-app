eaaefeb136f5b8399952b6b4c91bedbc3257a515
Felipe Cuevas <cuevas_castillo@hotmail.com>
2017-10-07 17:33:03 -0500

    I added new controller to manage categories records

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,49 @@
+class CategoriesController < ApplicationController
+    before_action :find_category, :only => [:show, :edit, :update]
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+ 
+  def create
+    @category = Category.new(category_params)
+ 
+    if @category.save
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    #TODO: SHOW A category IFORMATION
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    #TODO: destroy if don't have any relationship
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :code, :years_range, :laps, :city, :state, :phone, :email, :bood_type, :number)
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
+  end 
+end
