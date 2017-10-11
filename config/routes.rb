c533dfd5f28fd9f876ca7289c7fce90c4cbcec32
Felipe Cuevas <cuevas_castillo@hotmail.com>
2017-10-10 20:58:04 -0500

    I added new route to manage registers of rider

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
 
-  resources :riders
-  resources :categories
+  resources :riders do
+    resources :registers
+  end
+
+  resources :races, :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :races
 end
