26a623783d3c2f8801dbe18d314b21c6171f3103
Felipe Cuevas <cuevas_castillo@hotmail.com>
2017-10-10 21:01:23 -0500

    Run migration to registers race result

--- db/schema.rb
+++ db/schema.rb
@@ -10,51 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706035343) do
+ActiveRecord::Schema.define(version: 20171011002103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.string   "code"
-    t.string   "years_range"
-    t.integer  "laps"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.string "years_range"
+    t.integer "laps"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "races", force: :cascade do |t|
-    t.string   "title"
-    t.date     "date"
-    t.string   "photo"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "races", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.date "date"
+    t.string "photo"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "registers", force: :cascade do |t|
-    t.integer  "category"
-    t.integer  "race_id"
-    t.integer  "rider_id"
-    t.integer  "race_result"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["race_id"], name: "index_registers_on_race_id", using: :btree
-    t.index ["rider_id"], name: "index_registers_on_rider_id", using: :btree
+  create_table "registers", id: :serial, force: :cascade do |t|
+    t.integer "category"
+    t.integer "race_id"
+    t.integer "rider_id"
+    t.string "race_result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "points"
+    t.index ["race_id"], name: "index_registers_on_race_id"
+    t.index ["rider_id"], name: "index_registers_on_rider_id"
   end
 
-  create_table "riders", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.date     "birthdate"
-    t.string   "address"
-    t.string   "city"
-    t.string   "state"
-    t.string   "phone"
-    t.string   "email"
-    t.string   "bood_type"
-    t.integer  "number"
+  create_table "riders", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthdate"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "phone"
+    t.string "email"
+    t.string "bood_type"
+    t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
