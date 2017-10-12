430ac4cd342e1c89504b1c2b6a0272832521b9ef
Felipe Cuevas <cuevas_castillo@hotmail.com>
2017-10-12 13:40:16 -0500

    I added new column category to riders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011002103) do
+ActiveRecord::Schema.define(version: 20171011204240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20171011002103) do
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   add_foreign_key "registers", "races"
