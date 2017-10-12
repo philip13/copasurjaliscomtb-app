f100e776ebf43bde3a6c2f6ce92d69930aa79b1b
Felipe Cuevas <cuevas_castillo@hotmail.com>
2017-10-12 13:36:56 -0500

    app/controllers/application_controller.rb I move te method get_all_categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  private
+  def get_all_categories
+    @categories =  Category.all.map{|c| [] << c.name  << c.id}
+  end
 end
