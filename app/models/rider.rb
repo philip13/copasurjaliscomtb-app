fa1f82521f2c51bade34d6abbb2474fd0db950c8
Felipe Cuevas <cuevas_castillo@hotmail.com>
2017-10-23 10:35:29 -0500

    app/models/rider.rb I added new method to get riders by category id

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -3,4 +3,8 @@ class Rider < ApplicationRecord
   has_many :races, through: :registers
 
   accepts_nested_attributes_for :races
+
+  def self.get_riders_by_category_id category_id
+    self.where("category_id = ?", category_id)
+  end
 end
