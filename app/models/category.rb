61661cf72c30cc731291fb30b837f7a6cc42cae4
Felipe Cuevas <cuevas_castillo@hotmail.com>
2017-10-27 21:56:13 -0500

    app/models/category.rb new method to get all categories gy group order

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+
+  def self.get_all_categories_by_groups_order
+    self.order("categories.name asc, case when categories.group = 'SEGUNDO GRUPO'  then 1 when categories.group = 'PRIMER GRUPO' then 2 when categories.group = 'INFANTILES' then 3 end")
+  end
 end
