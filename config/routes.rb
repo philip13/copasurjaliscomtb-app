3c1f2e3851bb538b272efa643c52289d3bd38175
Felipe Cuevas <cuevas_castillo@hotmail.com>
2017-10-28 13:01:07 -0500

    config/routes.rb new route to search riders

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :registers
   end
 
+  get '/search_riders', to: 'riders#search_riders', as: 'search_riders'
+
   resources :races, :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
