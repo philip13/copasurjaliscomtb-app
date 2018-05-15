a2ead35cce146cb9aa6b6f787cba724f829b0a14
Felipe Cuevas <cuevas_castillo@hotmail.com>
2018-05-15 18:10:51 -0500

    app/controllers/home_controller.rb filter index page by year

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   before_action :get_all_categories, :only => [ :index]
   def index
     category_id = params[:category].blank? ? 1 : params[:category]
-    @riders_results = Register.get_general_clasification_by_category category_id
+    @riders_results = Register.get_general_clasification_by_category category_id, Date.new(2017, 1, 1), Date.new(2017, 12, 31)
+    # Register.get_general_clasification_by_category category_id
 
     respond_to do |format|
       format.html
