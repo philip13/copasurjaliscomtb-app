0707a05a8c74260f5a4bd9350abfe9c2871078d6
Felipe Cuevas <cuevas_castillo@hotmail.com>
2018-05-16 18:50:35 -0500

    app/models/register.rb I replace the includes to short code

--- app/models/register.rb
+++ app/models/register.rb
@@ -7,7 +7,6 @@ class Register < ApplicationRecord
   end
 
   def self.get_general_clasification_by_category category_id, start_date, end_date
- self.joins("INNER JOIN riders ON riders.id = registers.rider_id INNER JOIN races ON races.id = registers.race_id").select("riders.first_name as first_name, riders.last_name as last_name,  SUM (registers.points) as points_earned, registers.category, registers.rider_id ").where("registers.category = ? AND (races.date >= ? OR races.date <= ? )", category_id, start_date, end_date).group("registers.rider_id, riders.first_name,  riders.last_name, registers.category,  registers.rider_id").order("points_earned desc")
-    #self.joins(:rider).select("riders.first_name as first_name, riders.last_name as last_name,  SUM (registers.points) as points_earned, registers.category, registers.rider_id ").where("registers.category = ?", category_id).group("registers.rider_id, riders.first_name,  riders.last_name, registers.category,  registers.rider_id").order("points_earned desc")
+ self.joins(:rider, :race).select("riders.first_name as first_name, riders.last_name as last_name,  SUM (registers.points) as points_earned, registers.category, registers.rider_id ").where("registers.category = ? AND (races.date >= ? OR races.date <= ? )", category_id, start_date, end_date).group("registers.rider_id, riders.first_name,  riders.last_name, registers.category,  registers.rider_id").order("points_earned desc")
   end
 end
