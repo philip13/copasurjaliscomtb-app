7edf3768cd021e92d4d06f68cfec364fd7c08612
Felipe Cuevas <cuevas_castillo@hotmail.com>
2018-05-16 18:51:16 -0500

    app/controllers/home_controller.rb remove comment line

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
   def index
     category_id = params[:category].blank? ? 1 : params[:category]
     @riders_results = Register.get_general_clasification_by_category category_id, Date.new(2017, 1, 1), Date.new(2017, 12, 31)
-    # Register.get_general_clasification_by_category category_id
 
     respond_to do |format|
       format.html
