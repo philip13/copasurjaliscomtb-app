c727abc8576392c4cdc67c5b8a4be07c9349911a
Felipe Cuevas <cuevas_castillo@hotmail.com>
2018-05-30 20:38:32 -0500

    app/models/race.rb add new query get races by range date

--- app/models/race.rb
+++ app/models/race.rb
@@ -2,4 +2,9 @@ class Race < ApplicationRecord
   has_many :registers
   has_many :riders, through: :registers
   accepts_nested_attributes_for :riders
+
+
+  def self.get_races_of_range start_date, end_date
+    self.where("date >= ? AND date <= ? ", start_date, end_date).order("date")
+  end
 end
