ec4842a5a934cc0ef59482fa089d446d00256b9f
Felipe Cuevas <cuevas_castillo@hotmail.com>
2018-05-30 20:40:06 -0500

    app/controllers/home_controller.rb include new controllers to show result by ranking and race

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,42 @@
 class HomeController < ApplicationController
-  before_action :get_all_categories, :only => [ :index]
-  def index
+  before_action :get_all_categories, :only => [ :ranking, :race_results]
+  before_action :parse_year, :only => [:ranking, :races_of_year]
+  
+  def index  
+  end
+
+  def ranking
+    # @year = params[:year].blank? ? 2017 : params[:year].to_i
+
     category_id = params[:category].blank? ? 1 : params[:category]
-    @riders_results = Register.get_general_clasification_by_category category_id, Date.new(2017, 1, 1), Date.new(2017, 12, 31)
+    @riders_results = Register.get_general_clasification_by_category category_id, Date.new(@year, 1, 1), Date.new(@year, 12, 31)
 
     respond_to do |format|
       format.html
       format.js
     end
+  end
+
+  def races_of_year
+    @races = Race.get_races_of_range Date.new(@year, 1, 1), Date.new(@year, 12, 31)
+  end
+
+  def race_results
+    category_id = params[:category].blank? ? 1 : params[:category]
+
+    @race = Race.find params[:race_id] 
+
+    @riders_results = Register.get_race_result_by_category @race.id ,category_id unless @race.nil?
+  end
+
+  def road
+  end
+
+  def donation
+  end
+
+  private
+  def parse_year
+     @year = params[:year].blank? ? 2017 : params[:year].to_i
   end
 end
