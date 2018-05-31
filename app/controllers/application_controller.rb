1aa844fb2af36531d5a45b2136643043e71e37e4
Felipe Cuevas <cuevas_castillo@hotmail.com>
2018-05-30 21:52:14 -0500

    New metod to get picklist of categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,11 @@ class ApplicationController < ActionController::Base
     if action_name == 'ranking' && controller_name == 'home'
       @categories =  (Category.get_all_categories_by_groups_order(Date.new(params[:year].to_i, 12, 31) ) ).map{|c| [] << c.name  << c.id << c.active}
     else
-      @categories = Category.get_all_categories_by_groups_order(Date.today+1.day ).map{|c| [] << c.name  << c.id << c.active}
+      @categories = (Category.get_all_categories_by_groups_order(Date.today+1.day ) ).map{|c| [] << c.name  << c.id << c.active}
     end
+  end
+
+  def get_all_categories_picklist
+    @categories =  (Category.get_all_categories_by_groups_order(Date.today ) ).map{|c| [] << c.name  << c.id }
   end
 end
