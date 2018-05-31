4b95e14553110b0fe449758e54a326d876f77fb4
Felipe Cuevas <cuevas_castillo@hotmail.com>
2018-05-30 20:37:49 -0500

    commit app/models/register.rb new query to get results by race and category racer

--- app/models/register.rb
+++ app/models/register.rb
@@ -9,4 +9,9 @@ class Register < ApplicationRecord
   def self.get_general_clasification_by_category category_id, start_date, end_date
  self.joins(:rider, :race).select("riders.first_name as first_name, riders.last_name as last_name,  SUM (registers.points) as points_earned, registers.category, registers.rider_id ").where("registers.category = ? AND (races.date >= ? AND races.date <= ? )", category_id, start_date, end_date).group("registers.rider_id, riders.first_name,  riders.last_name, registers.category,  registers.rider_id").order("points_earned desc")
   end
+
+  def self.get_race_result_by_category race_id, category_id
+    self.select("riders.first_name as first_name, riders.last_name as last_name, registers.points as points_earned, registers.category, registers.rider_id").joins(:rider, :race).where("race_id = ? AND category = ?", race_id, category_id).order("points_earned desc")
+  end
+
 end
