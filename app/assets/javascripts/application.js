4075e33a4483b3f58df354ab487003f806dfd668
Felipe Cuevas <cuevas_castillo@hotmail.com>
2018-06-02 12:53:23 -0500

    app/assets/javascripts/application.js New script to load json appi google docs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,8 +30,40 @@ var auto_setting_points ={
   }
 }
 
+var fill_table_via_json = {
+  print_data_tbody: function(){
+    var $load_json =  $('[data-js-load-json]');
+    if (!$load_json.length) return;
+
+    var json_url= $('table').data('json-url');
+    console.log(json_url);
+
+  $.ajax({ 
+        type: 'GET', 
+        url: json_url, 
+        data: { get_param: 'value' }, 
+        dataType:'json',
+        success: function (data) { 
+          data.riders.forEach(function(element) {
+            $load_json.children('tbody').append(
+              `<tr>
+                <td>${element['lugar']}</td>
+                <td>${element['numero']}</td>
+                <td>${element['tiempo']}</td>
+                <td>${element['nombre']}</td>
+                <td>${element['apellidos']}</td>
+                <td>${element['equipo']}</td>
+              </tr>`
+              );
+          });
+        }
+    });
+  }
+}
+
 document.addEventListener("turbolinks:load", function() {
   $("#register_race_result").on('input', function() {
     auto_setting_points.to_race_result();
   });
+  fill_table_via_json.print_data_tbody();
 });
