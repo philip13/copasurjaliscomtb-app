065c9699cb376934bd58c9cf3f226ace60777e58
Felipe Cuevas <felipe@softwareallies.com>
2021-04-26 14:57:26 -0500

    app/models/category.rb update get all categories by groups order

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
 
   def self.get_all_categories_by_groups_order date_filter
-    self.where("created_at <= ?", date_filter).order("case when categories.group = 'SEGUNDO GRUPO'  then 1 when categories.group = 'PRIMER GRUPO' then 2 when categories.group = 'INFANTILES' then 3 end, categories.name asc")
+    self.where("active = TRUE", date_filter).order("case when categories.group = 'SEGUNDO GRUPO'  then 1 when categories.group = 'PRIMER GRUPO' then 2 when categories.group = 'INFANTILES' then 3 end, categories.name asc")
   end
 
 end
